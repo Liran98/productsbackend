ebed3bb5781a80f4744be79703f7bd09f4e6beca
liran <liran9800@gmail.com>
2023-01-12 14:27:08 +0200

    updated

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.use((error, req, res, next) => {
 
 
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster619.ys18umm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster619.ys18umm.mongodb.net/product_project?retryWrites=true&w=majority`)
 
   .then(() => {
     app.listen(process.env.PORT|| 5000, console.log(`connecting to localhost ${process.env.PORT} database working !!!`));
