08b4a549ac79e76b4a8cc52410616cf2f3844ec0
liran <liran9800@gmail.com>
2023-01-12 08:01:06 +0200

    updated

--- app.js
+++ app.js
@@ -11,19 +11,32 @@ const productsRoute = require('./routes/products-routes');
 const app = express();
 
 
+
+
+
 app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
-app.use(express.static(path.join('build')));
+
+
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
 
 
 app.use('/api/users', usersRoute);
 app.use('/api/products', productsRoute);
 
 
-app.use(( req, res, next) => {
-res.sendFile(path.resolve(__dirname,'build','index.html'));
-});
+
 
 
 app.use((error, req, res, next) => {
